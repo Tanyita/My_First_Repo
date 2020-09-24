7794a0c8cfe902252b9cc5d19b9a0a2007845709
Tanyita_bandita <tanja.milutinovic@gmail.com>
2020-09-24 13:43:10 -0700

    Adding annimation to the footer image (WIP)

--- 00_Mycode /Portfolio_website/HW18/index.js
+++ 00_Mycode /Portfolio_website/HW18/index.js
@@ -1,7 +1,6 @@
-//Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
@@ -12,8 +11,40 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
+
+
+
+
+
+
+
+
+function footer - muse() {
+    var a;
+    a = document.getElementsByClassName("footer-muse");
+    a.innerHTML = "footer-muse";
+    setTimeout(function() {
+        a.innerHTML = "&#xf256;";
+    }, 500);
+    setTimeout(function() {
+        a.innerHTML = "&#xf259;";
+    }, 1000);
+    setTimeout(function() {
+        a.innerHTML = "&#xf256;";
+    }, 1500);
 }
